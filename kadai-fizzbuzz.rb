3984a86d85e743a5addf25bf3255ea279281ec8a
yuuki.tamura <fxxk.the.rock@gmail.com>
2022-03-28 07:26:19 +0000

    add kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,19 +1,14 @@
 def fizzbuzz(num)
-      if num % 15 == 0
-          puts "FizzBuzz"
-      elsif num % 3 == 0
-          puts "Fizz"
-      elsif num % 5 == 0
-          puts "Buzz"
-      else
-          puts num
-      end
+  if num % 15 == 0
+  return "FizzBuzz"
+  elsif num % 3 == 0
+  return "Fizz"
+  elsif num % 5 == 0
+  return "Buzz"
+  else
+  return num
+  end
 end
-# 以下は動作確認用の記述です
-puts fizzbuzz(1)
-puts fizzbuzz(3)
-puts fizzbuzz(5)
-puts fizzbuzz(15)
 num_max = 100
 (1..num_max).each do |num|
   puts fizzbuzz(num)
